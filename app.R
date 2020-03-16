d67e7af9c2b178460663a14123657ccb8a0d92e0
Steven Fazzio <fazzios@gmail.com>
2020-03-16 00:20:06 -0500

    initial commit

---
+++ app.R
@@ -0,0 +1,124 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(lubridate)
+library(scales)
+
+all_data <- read_csv('data/processed_data.csv')
+
+regions <- all_data %>% 
+    filter(
+        statistic == 'confirmed',
+        date == ymd('2020-03-15')
+    ) %>% 
+    group_by(region) %>% 
+    summarise(num_people = sum(num_people)) %>% 
+    arrange(desc(num_people)) %>% 
+    select(region) %>% 
+    distinct() %>% 
+    pull()
+
+# Define UI for application that draws a histogram
+ui <- function(request) {
+    fluidPage(
+        
+        # Application title
+        titlePanel("COVID-19 Forecasting"),
+        
+        # Sidebar with a slider input for number of bins 
+        sidebarLayout(
+            
+            sidebarPanel(
+                dateRangeInput("date_range",
+                               "Date Range",
+                               start = ymd('2020-01-15'),
+                               end = today() + months(1)),
+                selectInput("region",
+                            "Region",
+                            choices = regions,
+                            selected = 'Non-China (International)'),
+                checkboxGroupInput("statistics",
+                                   "Statistics",
+                                   choices = c('confirmed', 'active', 
+                                               'recovered', 'deaths'),
+                                   selected = 'confirmed'),
+                numericInput('threshold',
+                             'Volume Threshold',
+                             value = 10,
+                             min = 1,
+                             step = 1)
+                
+            ),
+            
+            # Show a plot of the generated distribution
+            mainPanel(
+                plotOutput("forecastPlot")
+            )
+        ),
+        bookmarkButton()
+    )
+}
+
+# Define server logic required to draw a histogram
+server <- function(input, output, session) {
+
+    output$forecastPlot <- renderPlot({
+        
+        actual_data <- all_data %>% 
+            filter(
+                region == input$region,
+                num_people > 0
+            ) %>% 
+            mutate(type = 'actual') %>% 
+            select(type, date, statistic, num_people)
+        
+        stat_model <- function(df) {
+            lm(log(num_people) ~ date, data = df)
+        }
+        
+        date_range <- seq(input$date_range[[1]], input$date_range[[2]], by = '1 day')
+        
+        predicted_data <- actual_data %>% 
+            filter(num_people > input$threshold) %>% 
+            group_by(statistic) %>% 
+            nest() %>% 
+            mutate(
+                model = map(data, stat_model),
+                date = list(tibble(date = date_range)),
+                log_num_people = map2(model, date, ~predict(.x, newdata = .y))
+            ) %>% 
+            unnest(c(date, log_num_people)) %>%
+            mutate(
+                type = 'predicted',
+                num_people = exp(log_num_people)
+            ) %>%
+            select(type, date, statistic, num_people)
+        
+        df <- bind_rows(
+            actual_data,
+            predicted_data
+        ) %>% 
+            filter(statistic %in% input$statistics) 
+        
+        num_breaks <- ceiling(log10(max(df$num_people))) - floor(log10(min(df$num_people))) + 1
+        
+       df %>% 
+            ggplot(aes(x = date, y = num_people, color = statistic)) +
+            geom_line(aes(linetype = type)) +
+            scale_y_log10(label = label_comma(accuracy = 1), breaks = breaks_log(num_breaks)) +
+            scale_x_date(date_breaks = '1 week', date_labels = '%b %d') +
+            ggtitle(input$region)
+        
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server, enableBookmarking = "url")
---
+++ process_data.R
@@ -0,0 +1,92 @@
+library(tidyverse)
+library(lubridate)
+
+path <- '../COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/'
+
+us_state_abb <- tibble(
+  state_abb = state.abb,
+  state_name = state.name
+)
+ca_state_abb <- tibble(
+  state_abb = c('NL', 'PE', 'NS', 'NB', 'QC', 'ON', 'MB', 'SK', 'AB', 'BC', 'YT', 'NT', 
+                'NU'),
+  state_name = c('Newfoundland and Labrador', 'Prince Edward Island', 'Nova Scotia', 
+                 'New Brunswick', 'Quebec', 'Ontario', 'Manitoba', 'Saskatchewan',
+                 'Alberta', 'British Columbia', 'Yukon', 'Northwest Territories',
+                 'Nunavut')
+)
+state_abb <- bind_rows(us_state_abb, ca_state_abb)
+
+
+file_reader <- function(filename, path) {
+  date <- str_extract(filename, '\\d{2}-\\d{2}-\\d{4}') %>% 
+    mdy()
+  
+  str_c(path, filename) %>% 
+    read_csv() %>% 
+    mutate(date = date) %>% 
+    select(
+      state = `Province/State`,
+      country = `Country/Region`,
+      date,
+      confirmed = Confirmed,
+      deaths = Deaths,
+      recovered = Recovered
+    )
+}
+
+all_data <- list.files(path) %>% 
+  str_subset('.csv') %>% 
+  map(file_reader, path = path) %>% 
+  bind_rows() %>% 
+  replace_na(list(confirmed = 0, deaths = 0, recovered = 0)) %>% 
+  mutate(active = confirmed - (deaths + recovered)) %>% 
+  pivot_longer(-(1:3), names_to = 'statistic', values_to = 'num_people') %>% 
+  mutate(
+    country = if_else(country == 'Mainland China', 'China', country),
+    country = case_when(
+      country == 'Mainland China' ~ 'China',
+      country == 'Korea, South' ~ 'South Korea',
+      country == 'Republic of Korea' ~ 'South Korea',
+      TRUE ~ country
+    ),
+    state = case_when(
+      state == 'Virgin Islands, U.S.' ~ 'Virgin Islands',
+      str_detect(state, 'Princess') ~ 'Cruise Ship',
+      TRUE ~ state
+    ),
+    state = str_replace(state, '.*, ', '')
+  ) 
+
+by_state <- all_data %>% 
+  filter(!is.na(state)) %>% 
+  mutate(region = str_c(state, ', ', country, ' (State/Province)')) %>% 
+  group_by(region_type = 'state', region, date, statistic) %>% 
+  summarise(num_people = sum(num_people))
+
+by_country <- all_data %>% 
+  mutate(region = str_c(country, ' (Country)')) %>% 
+  group_by(region_type = 'country', region, date, statistic) %>% 
+  summarise(num_people = sum(num_people))
+
+non_china <- all_data %>% 
+  filter(country != 'China') %>% 
+  mutate(region = 'Non-China (International)') %>% 
+  group_by(region_type = 'international', region, date, statistic) %>% 
+  summarise(num_people = sum(num_people))
+
+global <- all_data %>% 
+  mutate(region = 'Global (Internationl)') %>% 
+  group_by(region_type = 'international', region, date, statistic) %>% 
+  summarise(num_people = sum(num_people))
+
+processed_data <- bind_rows(
+  by_state,
+  by_country,
+  non_china,
+  global
+)
+
+write_csv(processed_data, 'data/processed_data.csv')
+
+
